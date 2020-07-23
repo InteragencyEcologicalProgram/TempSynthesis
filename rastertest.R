7d731669bef158fb0adcc7d5cecb9e409deb60e6
rosehartman <rosehartman@gmail.com>
2020-07-23 08:00:46 -0700

    Cluster analysis and local regression code

---
+++ rastertest.R
@@ -0,0 +1,39 @@
+
+
+if (!require("rspatial")) devtools::install_github('rspatial/rspatial')
+library(rspatial)
+counties <- sp_data('counties')
+p <- sp_data('precipitation')
+head(p)
+
+p$pan <- rowSums(p[,6:17])
+
+plot(counties)
+
+alb <- CRS("+proj=aea +lat_1=34 +lat_2=40.5 +lat_0=0 +lon_0=-120 +x_0=0 +y_0=-4000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs")
+sp <- p
+coordinates(sp) = ~ LONG + LAT
+crs(sp) <- "+proj=longlat +datum=NAD83"
+spt <- spTransform(sp, alb)
+ctst <- spTransform(counties, alb)
+
+library( spgwr )
+## NOTE: This package does not constitute approval of GWR
+## as a method of spatial analysis; see example(gwr)
+bw <- gwr.sel(pan ~ ALT, data=spt)
+
+r <- raster(ctst, res=10000)
+r2 <- rasterize(ctst, r)
+newpts <- rasterToPoints(r2)
+
+g <- gwr(pan ~ ALT, data=spt, bandwidth=bw, fit.points=newpts[, 1:2])
+g
+
+
+slope <- r
+intercept <- r
+slope[!is.na(slope)] <- g$SDF$ALT
+intercept[!is.na(intercept)] <- g$SDF$'(Intercept)'
+s <- stack(intercept, slope)
+names(s) <- c('intercept', 'slope')
+plot(s)
